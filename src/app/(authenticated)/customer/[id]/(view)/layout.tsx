78e03106c50b8b4cd0b81b8f66159cc91965e46c
Natsumi.H <natsmy.1211@gmail.com>
2024-02-13 10:34:25 +0800

    data fetch update

--- src/app/(authenticated)/customer/[id]/(view)/layout.tsx
+++ src/app/(authenticated)/customer/[id]/(view)/layout.tsx
@@ -8,14 +8,12 @@ const getCustomer = async (id: string) => {
     const res = await fetch(
       `${process.env.NEXT_PUBLIC_BASE_URL}/api/customer/${id}`
     );
-
     if (!res.ok) {
       // This will activate the closest `error.js` Error Boundary
       throw new Error("Failed to fetch data");
     }
-
     const data = await res.json();
-
+    console.log(data);
     return data;
   } catch (e) {
     console.log(e);
