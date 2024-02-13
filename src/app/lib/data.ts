3326cc5dbfda4207658af26e408ced30408f3931
Natsumi.H <natsmy.1211@gmail.com>
2024-02-13 13:49:30 +0800

    data fetch

--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -1,10 +1,11 @@
 export const getCustomer = async (id: string) => {
     console.log(id);
-    
+
   try {
     const res = await fetch(
       `https://smartinvoice-dev.vercel.app/api/customer/${id}/`
     );
+    console.log(res); // ここでレスポンスをログに出力
     if (!res.ok) {
       throw new Error("Failed to fetch data");
     }
