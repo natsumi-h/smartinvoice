d97e0dd3079fb228659f2c85bc125f08a62d1308
Natsumi.H <natsmy.1211@gmail.com>
2024-02-18 01:09:10 +0800

    puppeteer debug

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,7 @@ const nextConfig = {
     ],
   },
   experimental: {
-    serverComponentsExternalPackages: ["puppeteer-core", "@sparticuz/chromium"],
+    serverComponentsExternalPackages: ["puppeteer-core", "@sparticuz/chromium-min"],
   },
   // async headers() {
   //   return [
