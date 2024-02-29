0283b0a80f786bfab1e1c72ca0fc38035f732e57
Natsumi.H <natsmy.1211@gmail.com>
2024-02-29 15:27:05 +0800

    not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,21 @@
 import Link from "next/link";
 
+import { Title, Text, Button, Container, Group, Anchor } from "@mantine/core";
+import classes from "./NotFound.module.css";
+
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
+    <Container className={classes.root}>
+      <div className={classes.label}>404</div>
+      <Text c="dimmed" size="lg" ta="center" className={classes.description}>
+        You may have mistyped the
+        address, or the page has been moved to another URL.
+      </Text>
+      <Group justify="center">
+        <Anchor href="/" component={Link}>
+          Take me back to home page
+        </Anchor>
+      </Group>
+    </Container>
   );
 }
